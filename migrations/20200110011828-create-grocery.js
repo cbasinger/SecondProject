2a675067c3e796ee3ff3f09f6d043f32304b3e20
Barbie <barbara.kelly892@gmail.com>
2020-01-18 16:16:00 -0500

    changed migrations

--- migrations/20200110011828-create-grocery.js
+++ migrations/20200110011828-create-grocery.js
@@ -11,14 +11,6 @@ module.exports = {
       groceryitem: {
         type: Sequelize.STRING
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
     });
   },
   down: (queryInterface, Sequelize) => {
