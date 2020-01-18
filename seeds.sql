afb37d5fd93a60a9580d69b5ed03700668c5b033
Barbie <barbara.kelly892@gmail.com>
2020-01-18 13:08:10 -0500

    connecting to heroku database and creating seed data

---
+++ seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO todos (todoitem) 
+VALUES ("to do item 1"),
+("to do item 2"),
+("to do item 3");
+
+INSERT INTO groceries (groceryitem)
+VALUES ("grocery item 1"),
+("grocery item 2"),
+("grocery item 3");
