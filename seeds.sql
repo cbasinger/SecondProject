6f85da261da72149387de7c6472fd60f61221b82
Barbie <barbara.kelly892@gmail.com>
2020-01-18 13:31:36 -0500

    debugging seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -1,9 +1,9 @@
 INSERT INTO todos (todoitem) 
 VALUES ('to do item 1',
 ('to do item 2'),
-('to do item 3');
+('to do item 3')
 
 INSERT INTO groceries (groceryitem)
 VALUES ('grocery item 1'),
 ('grocery item 2'),
-('grocery item 3');
+('grocery item 3')
