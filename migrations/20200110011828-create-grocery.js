44529d8e8aab8423cab633f2520131e89b3e115a
Barbie <barbara.kelly892@gmail.com>
2020-01-18 19:56:10 -0500

    testing removal of migrations and models/index.js, changes to index.ejs bootstrap cards

--- migrations/20200110011828-create-grocery.js
+++ migrations/20200110011828-create-grocery.js
@@ -1,19 +1,19 @@
-'use strict';
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('groceries', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      groceryitem: {
-        type: Sequelize.STRING
-      },
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('groceries');
-  }
-};
+// 'use strict';
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//     return queryInterface.createTable('groceries', {
+//       id: {
+//         allowNull: false,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         type: Sequelize.INTEGER
+//       },
+//       groceryitem: {
+//         type: Sequelize.STRING
+//       },
+//     });
+//   },
+//   down: (queryInterface, Sequelize) => {
+//     return queryInterface.dropTable('groceries');
+//   }
+// };
