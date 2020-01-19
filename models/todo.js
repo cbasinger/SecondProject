76627bfc4fc0b0edfd85127202a3768c24514d5d
Barbie <barbara.kelly892@gmail.com>
2020-01-18 19:42:30 -0500

    moving todo &grocery into bootstrap cards and removing models

--- models/todo.js
+++ models/todo.js
@@ -1,10 +1,10 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const todo = sequelize.define('todo', {
-    todoitem: DataTypes.STRING
-  }, {});
-  todo.associate = function(models) {
-    // associations can be defined here
-  };
-  return todo;
-};
+// 'use strict';
+// module.exports = (sequelize, DataTypes) => {
+//   const todo = sequelize.define('todo', {
+//     todoitem: DataTypes.STRING
+//   }, {});
+//   todo.associate = function(models) {
+//     // associations can be defined here
+//   };
+//   return todo;
+// };
