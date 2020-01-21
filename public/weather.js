4a42db46cddf2c29d92b776652a840f5614d09c8
Chelsea Basinger <Chelsea@ChelseasMacbook.attlocal.net>
2020-01-21 12:53:10 -0500

    weather again

--- public/weather.js
+++ public/weather.js
@@ -1,6 +1,6 @@
 /* const axios = require("axios");  */
 const Weather = [];
-const weatherCard = document.getElementById('card-body');
+const weatherCard = document.getElementById('weather-body');
 
 const getWeather = function(){
 	axios({
@@ -33,57 +33,3 @@ const getWeather = function(){
 getWeather()
 
 	
-/* const unirest = require('unirest');
-const express = require('express');
-const app = express();
-const req2 = unirest("GET", "https://dark-sky.p.rapidapi.com/33.9526,-84.5499");
-const Weather = [];
-const weatherCard = document.getElementById('weather-body');
-
-
-req2.query({
-	"lang": "en",
-    "units": "auto",
-    "exclude": "minutely%2C hourly%2C daily%2C flags"
-});
-
-req.headers({
-	"x-rapidapi-host": "dark-sky.p.rapidapi.com",
-	"x-rapidapi-key": "8fcd0baf41msh43380c9a5223fd7p18e2c5jsn53d5da255ddd"
-});
-
-app.get('/', function(req, res) {
-    res.render('index');
-});
-
-function getWeather(){
-req.end(function (res) {
-	const weatherObject = {};
-	weatherObject.name = "Local Weather";
-	weatherObject.currently = res.body.currently.summary;
-	weatherObject.temp = res.body.currently.temperature;
-	weatherObject.wind = res.body.currently.windSpeed;
-	weatherObject.alerts = res.body.alerts;
-
-	Weather.push(weatherObject);
-
-	
-	weatherCard.appendChild(Weather);
-	
-
-	/* if (res.error) throw new Error(res.error); */
-	
-
-	/* console.log(res.body.currently);
-	console.log(res.body.alerts); */
-/* });
-};
-getWeather();
-
-const createWeathercard = function (){
-
-}
- */
-/* app.listen(3000, () => {
-    console.log('Listening on port :3000');
-}) */ 
