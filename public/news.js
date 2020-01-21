b8cee05147f6dc2a71e049533def083e09e16ee3
Chelsea Basinger <Chelsea@ChelseasMacbook.attlocal.net>
2020-01-21 13:20:19 -0500

    news

--- public/news.js
+++ public/news.js
@@ -14,36 +14,10 @@ const getNews = function(){
     newsObject.description = response.data.articles[0].description;
     newsObject.content = response.data.articles[0].content;
     newsObject.date = response.data.articles[0].publishedAt;
-    newsCard.innerHTML = newsObject.title + newsObject.source + newsObject.content;
+    newsCard.innerHTML = "Title: " + newsObject.title + "Source: " + newsObject.source + " " + newsObject.content;
     console.log(newsObject);
     })
 };
 getNews();
 
 
-/* req.query({
-    language: 'en',
-    country: 'us'
-});
-
-function getNews() {
-req.end(function (res) {
-    const newsObject = {};
-    newsObject.name = "Top Headlines";
-    newsObject.title = res.body.articles[0].title;
-    newsObject.source = res.body.articles[0].source;
-    newsObject.description = res.body.articles[0].description;
-    newsObject.content = res.body.articles[0].content;
-    newsObject.date = res.body.articles[0].publishedAt;
-
-    News.push(newsObject);
-    newsCard.appendChild(News);
-});
-};
-getNews();
-
-
-
-    
-
- */
