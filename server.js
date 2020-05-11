f284b27c6d2f55c5890b74b12dea8049d0e38f1e
Chelsea Basinger <chelsea.basinger@gmail.com>
2020-05-11 17:30:23 -0400

    ugh

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(cookieParser())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
-app.use( express.static( "public" ) );
+app.use('/public', express.static('public'));
 
 
 
